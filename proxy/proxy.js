aab9658e9e42def5462608627bf291fe2d07b0b5
Kenton Vizdos (MainPC) <kvizdos@gmail.com>
2019-08-08 16:30:27 -0400

    ssl

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -46,7 +46,7 @@ const confirmAuth = (host, url, req) => {
         return null;
     } 
 
-    return "http://127.0.0.1:" + _CONF.ports.unauthed;
+    return "https://127.0.0.1:" + _CONF.ports.unauthed;
 
     // // return null;
     // return mainApp == "auth" ? null : {url: "http://127.0.0.1/auth/verify"};
