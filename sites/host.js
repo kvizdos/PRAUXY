27eec5417ccceb83776cc90fa90445764ae816cc
Kenton Vizdos (MainPC) <kvizdos@gmail.com>
2020-05-24 11:44:07 -0400

    Branding

--- sites/host.js
+++ sites/host.js
@@ -16,7 +16,7 @@ app.get("/*", (req, res) => {
     const isNotCustom = regex.test(req.headers.host);
 
     const sendData = (rootDir) => {
-        if(req.params[0] != "" && req.params[0].indexOf("html") == -1) {
+        if(req.params[0] != "" && req.params[0] != "/" && req.params[0].indexOf("html") == -1) {
             console.log("here = " + (req.params[0] != ""));
             res.sendFile(__dirname + `/data/${rootDir}/${req.params[0]}`, (err) => {
                 if(err) {
